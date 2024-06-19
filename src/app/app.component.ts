2e43eff0944ddef94bd7d028c2ecc230efd44aee
DianaSanchezOrdonez <dianaordonez1998@gmail.com>
2024-06-19 15:37:53 -0500

    feat: add toolbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { CardComponent } from './shared/components/card/card.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { SearchComponent } from './shared/components/search/search.component';
+import { ToolbarComponent } from './shared/components/toolbar/toolbar.component';
 
 @Component({
   selector: 'app-root',
@@ -17,6 +18,7 @@ import { SearchComponent } from './shared/components/search/search.component';
     SearchComponent,
     NavListComponent,
     CardComponent,
+    ToolbarComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
