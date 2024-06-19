d5047e7c3920171a6f786c6317b08a10c6bbcb58
DianaSanchezOrdonez <dianaordonez1998@gmail.com>
2024-06-19 10:57:03 -0500

    feat: add routing on sidebar

--- src/app/shared/components/nav-list/nav-list.component.ts
+++ src/app/shared/components/nav-list/nav-list.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list'
+import { MatListModule } from '@angular/material/list';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-list',
   standalone: true,
-  imports: [MatSidenavModule, MatIconModule, MatListModule],
+  imports: [RouterLink, MatSidenavModule, MatIconModule, MatListModule],
   templateUrl: './nav-list.component.html',
   styleUrl: './nav-list.component.css',
 })
--- src/app/shared/shared.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { NavListComponent } from "./components/nav-list/nav-list.component";
-
-@NgModule({
-  declarations: [],
-  imports: [NavListComponent],
-  exports: [NavListComponent]
-})
-export class ShareModule {}
