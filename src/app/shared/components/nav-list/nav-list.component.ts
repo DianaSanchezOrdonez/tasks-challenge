5242eaa5a2dd48ae3bf43969e1903e9412531082
DianaSanchezOrdonez <dianaordonez1998@gmail.com>
2024-06-19 14:30:06 -0500

    chore: add styles to nav-list

--- src/app/shared/components/nav-list/nav-list.component.ts
+++ src/app/shared/components/nav-list/nav-list.component.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-list',
   standalone: true,
-  imports: [RouterLink, MatSidenavModule, MatIconModule, MatListModule],
+  imports: [CommonModule, RouterLink, MatSidenavModule, MatIconModule, MatListModule],
   templateUrl: './nav-list.component.html',
   styleUrl: './nav-list.component.css',
 })
-export class NavListComponent {}
+export class NavListComponent {
+  constructor(private router: Router) {}
+
+  isRouteActive(route: string): boolean {
+    return this.router.url === route;
+  }
+}
