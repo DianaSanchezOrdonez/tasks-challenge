9861722acaea3672bd4666600a572b1fce823256
DianaSanchezOrdonez <dianaordonez1998@gmail.com>
2024-06-20 13:11:55 -0500

    chore: create avatar component

---
+++ src/app/core/components/avatar/avatar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-avatar',
+  standalone: true,
+  imports: [],
+  templateUrl: './avatar.component.html',
+  styleUrl: './avatar.component.css'
+})
+export class AvatarComponent {
+
+}
