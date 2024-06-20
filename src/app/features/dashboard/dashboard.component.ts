9d4516bef0fd97a75c8e4b37c8c508569b692b7b
DianaSanchezOrdonez <dianaordonez1998@gmail.com>
2024-06-20 17:26:48 -0500

    feat: get tasks by status

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,60 +1,41 @@
 import { Component } from '@angular/core';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { Task } from './models/task.model';
 import { DataService } from '../../core/services/data.service';
 import { CardComponent } from '../../core/components/card/card.component';
 import { HttpClient } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
 import { StatusEnum } from './models/task.enums';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, CardComponent],
+  imports: [CardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
-  // tasks: Task[] = []
-  status: string = 'BACKLOG';
-  tasks: Task[] = [
-    {
-      id: 'c5baa84e-7671-4d93-8842-c594a706d360',
-      name: 'Ticket1',
-      tags: ['ANDROID', 'REACT'],
-      status: StatusEnum.BACKLOG,
-      assignee: {
-        id: '703de395-1d49-4471-aafa-d990dcf32cd1',
-        fullName: 'Grace Stone',
-        email: 'gstone@fake.com',
-        type: 'CANDIDATE',
-        avatar: 'https://avatars.dicebear.com/api/initials/gs.svg',
-        createdAt: '2022-12-28T15:33:06.078Z',
-        updatedAt: '2022-12-28T15:33:06.079Z',
-      },
-      creator: {
-        id: '703de395-1d49-4471-aafa-d990dcf32cd1',
-        fullName: 'Grace Stone',
-        email: 'gstone@fake.com',
-        type: 'CANDIDATE',
-        avatar: 'https://avatars.dicebear.com/api/initials/gs.svg',
-        createdAt: '2022-12-28T15:33:06.078Z',
-        updatedAt: '2022-12-28T15:33:06.079Z',
-      },
-      position: 1,
-      dueDate: '2024-06-16T19:01:23.566Z',
-      pointEstimate: '8',
-      createdAt: '2024-06-11T19:57:17.611Z',
-    },
-  ];
+  workingTasks: Task[] = [];
+  inProgressTasks: Task[] = [];
+  completedTasks: Task[] = [];
 
   constructor(private dataService: DataService, private http: HttpClient) {}
 
   ngOnInit() {
-    const params = { key: 'status', value: this.status };
-
-    this.dataService.getTasks(params).subscribe((result) => {
-      this.tasks = result;
+    this.loadTasksByStatus(StatusEnum.BACKLOG).subscribe((result) => {
+      this.workingTasks = result;
     });
+
+    this.loadTasksByStatus(StatusEnum.IN_PROGRESS).subscribe((result) => {
+      this.inProgressTasks = result;
+    });
+
+    this.loadTasksByStatus(StatusEnum.DONE).subscribe((result) => {
+      this.completedTasks = result;
+    });
+  }
+
+  loadTasksByStatus(status: StatusEnum): Observable<Task[]> {
+    const params = { key: 'status', value: status };
+    return this.dataService.getTasks(params);
   }
 }
