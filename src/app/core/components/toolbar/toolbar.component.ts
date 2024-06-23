59bceafcff07fc58f99aac21cfd7fc6cfc2f1869
DianaSanchezOrdonez <dianaordonez1998@gmail.com>
2024-06-22 21:53:03 -0500

    chore: add button selected function on toolbar

--- src/app/core/components/toolbar/toolbar.component.ts
+++ src/app/core/components/toolbar/toolbar.component.ts
@@ -1,15 +1,34 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar'
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { CustomIconsComponent } from '../custom-icons/custom-icons.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, CustomIconsComponent],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    CustomIconsComponent,
+    MatButtonModule,
+    MatDialogModule,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.css'
+  styleUrl: './toolbar.component.css',
 })
 export class ToolbarComponent {
+  activeButton: string = 'grid-view';
+  readonly dialog = inject(MatDialog);
 
+  openDialog() {
+    this.dialog.open(ModalComponent);
+  }
+
+  setActiveButton(buttonId: string) {
+    this.activeButton = buttonId;
+  }
 }
