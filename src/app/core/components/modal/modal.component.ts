e850962392699e7bc04cc39ca6cd171eda1c8099
DianaSanchezOrdonez <dianaordonez1998@gmail.com>
2024-06-24 09:53:58 -0500

    chore: handling custom selectors into modal component

--- src/app/core/components/modal/modal.component.ts
+++ src/app/core/components/modal/modal.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { CommonModule } from '@angular/common';
 import { CustomSelectComponent } from '../custom-select/custom-select.component';
+import { SelectOption } from '../custom-select/custom-select.model';
+import { DataService } from '../../services/data.service';
+import { User } from '../../../features/dashboard/models/user.model';
+import { TaskInput } from '../../services/models/task.input';
 
-type DropdownKeys = 'estimate' | 'assignee';
+type DropdownKeys = 'estimate' | 'assignee' | 'label' | 'dueDate';
 
 @Component({
   selector: 'app-modal',
@@ -19,27 +29,90 @@ type DropdownKeys = 'estimate' | 'assignee';
     MatFormFieldModule,
     ReactiveFormsModule,
     MatInputModule,
-    CustomSelectComponent
+    CustomSelectComponent,
   ],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css',
 })
 export class ModalComponent {
+  createTaskForm: FormGroup;
+
+  constructor(private dataService: DataService, private fb: FormBuilder) {
+    this.createTaskForm = this.fb.group({
+      title: ['', Validators.required],
+      estimate: ['', Validators.required],
+      assignee: ['', Validators.required],
+      label: ['', Validators.required],
+    });
+  }
+
   dropdowns: { [key in DropdownKeys]: boolean } = {
     estimate: false,
-    assignee: false
+    assignee: false,
+    label: false,
+    dueDate: false
   };
 
-  selectedOptions: { [key in DropdownKeys]?: any } = {};
+  selectedOptions: { [key in DropdownKeys]?: SelectOption } = {};
+  assigneesList: User[] = [];
+  assigneesOptions: SelectOption[] = [];
 
   toggleDropdown(select: DropdownKeys) {
     this.dropdowns[select] = !this.dropdowns[select];
+
+    if (select === 'assignee' && this.assigneesList.length) {
+      this.assigneesList.forEach((user) =>
+        this.assigneesOptions.push({
+          label: user.fullName,
+          value: user.id,
+          avatarUrl: user.avatar,
+        })
+      );
+    }
   }
 
-  selectOption(select: DropdownKeys, option: any) {
+  selectOption(select: DropdownKeys, option: SelectOption) {
+    console.log('select => ', select);
     this.selectedOptions[select] = option;
-    console.log(`Selected ${option.label} for ${select}`);
+    this.createTaskForm.get(select)?.setValue(option.value);
     this.dropdowns[select] = false;
   }
 
+  ngOnInit() {
+    this.dataService.getUsers().subscribe({
+      next: (result) => {
+        this.assigneesList = result.data.users;
+      },
+      error: (error) => {
+        console.error('Error fetching users', error);
+      },
+    });
+  }
+
+  createTask(input: TaskInput) {
+    this.dataService.createTask(input).subscribe({
+      next: (result) => {
+        if (result.data) {
+          console.log('Task created:', result.data.createTask);
+        }
+      },
+      error: (error) => {
+        console.error('There was an error creating the task:', error);
+      },
+    });
+  }
+
+  onSubmit() {
+    console.warn(this.createTaskForm.value);
+    const { title, estimate, assignee, label } = this.createTaskForm.value;
+
+    // this.createTask({
+    //   name: title,
+    //   pointEstimate: estimate,
+    //   assigneeId: assignee,
+    //   dueDate: new Date().toDateString(),
+    //   status: StatusEnum.BACKLOG,
+    //   tags: [TagsEnum.NODE_JS],
+    // });
+  }
 }
