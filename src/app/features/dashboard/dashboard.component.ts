a1888257638930a96a68d97872e09644878ae858
DianaSanchezOrdonez <dianaordonez1998@gmail.com>
2024-07-03 00:07:22 -0500

    feat: add dashboard and tasks pages

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,59 +1,50 @@
 import { Component } from '@angular/core';
-import { Task } from './models/task.model';
-import { CardComponent } from '../../core/components/card/card.component';
-import { StatusEnum } from './models/task.enums';
+import { Task } from '../../core/services/models/task.model';
+import { StatusEnum } from '../../core/services/models/task.enums';
 import { Observable, map } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
-import { MatCardModule } from '@angular/material/card';
 import { ToolbarComponent } from '../../core/components/toolbar/toolbar.component';
 import { SearchComponent } from '../../core/components/search/search.component';
 import { Store } from '@ngrx/store';
-import { listTasks, resetLoading } from '../../core/store/task.actions';
-import { TaskState } from '../../core/store/task.typos';
-import {
-  selectAllTasks,
-  selectFilteredTasks,
-  selectTaskError,
-  selectTaskLoading,
-} from '../../core/store/task.selectors';
+import { listTasks } from '../../core/store/tasks/task.actions';
+import { TaskState } from '../../core/store/tasks/task.state';
+import { selectFilteredTasks } from '../../core/store/tasks/task.selectors';
+import { GridViewComponent } from '../../grid-view/grid-view.component';
+import { ListViewComponent } from '../../list-view/list-view.component';
+import { DataService } from '../../core/services/data.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
     CommonModule,
-    CardComponent,
-    NgxSkeletonLoaderModule,
-    MatCardModule,
     ToolbarComponent,
     SearchComponent,
+    GridViewComponent,
+    ListViewComponent,
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
   filterParam = '';
-
-  allTasks$: Observable<Task[]>;
-  loading$!: Observable<boolean>;
-  error$: Observable<any>;
+  viewSelection: string = 'grid-view';
 
   backlogTasks$!: Observable<Task[]>;
   inProgressTasks$!: Observable<Task[]>;
   doneTasks$!: Observable<Task[]>;
 
-  constructor(private store: Store<{ taskState: TaskState }>) {
-    this.allTasks$ = this.store.select(selectAllTasks);
-    this.loading$ = this.store.select(selectTaskLoading);
-    this.error$ = this.store.select(selectTaskError);
+  profileUrl: string | null = null;
 
-    this.setTaskStreams();
-  }
+  constructor(
+    private store: Store<{ taskState: TaskState }>,
+    private dataService: DataService
+  ) {}
 
   ngOnInit() {
-    this.store.dispatch(listTasks({ }));
-    this.store.dispatch(resetLoading())
+    this.store.dispatch(listTasks({ filter: {} }));
+    this.setTaskStreams();
+    this.getUserInfo();
   }
 
   receivedFilter(filter: string) {
@@ -87,5 +78,20 @@ export class DashboardComponent {
           tasks.filter((task) => task.name.includes(this.filterParam))
         )
       );
+  }
+
+  handleTasksView(buttonId: string) {
+    this.viewSelection = buttonId;
+  }
+
+  getUserInfo() {
+    this.dataService.getProfile().subscribe({
+      next: (result) => {
+        this.profileUrl = result.data.profile.avatar;
+      },
+      error: (error) => {
+        console.error('Error fetching profile', error);
+      },
+    });
   }
 }
--- src/app/features/tasks/tasks.component.ts
+++ src/app/features/tasks/tasks.component.ts
@@ -1,12 +1,112 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { TaskState } from '../../core/store/tasks/task.state';
+import { selectFilteredTasks } from '../../core/store/tasks/task.selectors';
+import { Observable, map } from 'rxjs';
+import { Task } from '../../core/services/models/task.model';
+import { listTasks } from '../../core/store/tasks/task.actions';
+import { CommonModule } from '@angular/common';
+import { SearchComponent } from '../../core/components/search/search.component';
+import { ToolbarComponent } from '../../core/components/toolbar/toolbar.component';
+import { StatusEnum } from '../../core/services/models/task.enums';
+import { ListViewComponent } from '../../list-view/list-view.component';
+import { GridViewComponent } from '../../grid-view/grid-view.component';
+import { DataService } from '../../core/services/data.service';
+import { User } from '../../core/services/models/user.model';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
+  imports: [
+    CommonModule,
+    SearchComponent,
+    ToolbarComponent,
+    ListViewComponent,
+    GridViewComponent,
+  ],
 })
 export class TasksComponent {
-  
+  filterParam = '';
+  viewSelection: string = 'grid-view';
+
+  backlogTasks$!: Observable<Task[]>;
+  inProgressTasks$!: Observable<Task[]>;
+  doneTasks$!: Observable<Task[]>;
+
+  user!: User;
+
+  constructor(
+    private dataService: DataService,
+    private store: Store<{ taskState: TaskState }>
+  ) {
+    this.dataService.getProfile().subscribe({
+      next: (result) => {
+        this.user = result.data.profile;
+      },
+      error: (error) => {
+        console.error('Error fetching profile', error);
+      },
+    });
+  }
+
+  ngOnInit() {
+    this.store.dispatch(listTasks({ filter: {} }));
+    this.setTaskStreams();
+  }
+
+  receivedFilter(filter: string) {
+    this.filterParam = filter;
+    this.setTaskStreams();
+  }
+
+  setTaskStreams() {
+    this.backlogTasks$ = this.store
+      .select(
+        selectFilteredTasks({
+          key: 'status',
+          value: StatusEnum.BACKLOG,
+        })
+      )
+      .pipe(
+        map((tasks) =>
+          tasks.filter(
+            (task) =>
+              task.assignee.id === this.user.id &&
+              task.name.includes(this.filterParam)
+          )
+        )
+      );
+
+    this.inProgressTasks$ = this.store
+      .select(
+        selectFilteredTasks({ key: 'status', value: StatusEnum.IN_PROGRESS })
+      )
+      .pipe(
+        map((tasks) =>
+          tasks.filter(
+            (task) =>
+              task.assignee.id === this.user.id &&
+              task.name.includes(this.filterParam)
+          )
+        )
+      );
+
+    this.doneTasks$ = this.store
+      .select(selectFilteredTasks({ key: 'status', value: StatusEnum.DONE }))
+      .pipe(
+        map((tasks) =>
+          tasks.filter(
+            (task) =>
+              task.assignee.id === this.user.id &&
+              task.name.includes(this.filterParam)
+          )
+        )
+      );
+  }
+
+  handleTasksView(buttonId: string) {
+    this.viewSelection = buttonId;
+  }
 }
